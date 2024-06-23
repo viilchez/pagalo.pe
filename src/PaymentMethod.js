e648c3184964b06140a21295ab15f64cf10e4387
viilchez <167554276+viilchez@users.noreply.github.com>
2024-06-23 05:43:56 +0000

    Mensaje de commit

--- src/PaymentMethod.js
+++ src/PaymentMethod.js
@@ -4,6 +4,9 @@ import Main from './Main';
 import Aside from './Aside';
 import Footer from './layout/Footer';
 import Sidebar from './layout/Sidebar';
+import agente from './assets/images.png';
+import yape from './assets/yape.png';
+import pagoEfectivo from './assets/pago-efectivo.avif';
 
 const PaymentMethod = () => (
   <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#ffffff' }}>
@@ -12,6 +15,24 @@ const PaymentMethod = () => (
       <h1>Método de Pago</h1>
       <h2>Seleccionar Método de Pago</h2>
     </div>
+    <div className="payment-info-rectangle">
+        <h1>Información</h1>
+        <h2>¿Habrá algún cargo adicional?</h2>
+        <h3>No, solo se cargará a tu tarjeta el importe de la tasa o servicios que has seleccionado. No se aplicarán cargos adicionales por comisiones u otros gastos financieros. </h3>
+    </div>
+    <div className="method">
+      <p>Tarjeta de Crédito o Débito</p>
+      <h1>Otros Métodos</h1>
+      <a className="method-multired" >
+      <img src={agente} alt="Agente MultiRed" height="150" style={{ marginLeft: '0px' }} />
+      </a>
+      <a className="method-yape" >
+        <img src={yape} alt="Yape" height="160" style={{ marginLeft: '0px' }} />
+        </a>
+        <a className="method-pago-efectivo" >
+        <img src={pagoEfectivo} alt="Pago Efectivo" width="600" style={{ marginLeft: '0px' }} />
+        </a>   
+      </div>
     <div className="payment-rectangle">
       <form className="payment-form">
         <label htmlFor="cardNumber">Número de Tarjeta:</label>
@@ -27,9 +48,6 @@ const PaymentMethod = () => (
         <input type="text" id="cvv" name="cvv" />
         <button type="submit">Pagar</button>
       </form>
-      <div className="paymenbt-rectangle">
-
-      </div>
     </div>
     <Main />
     <Aside />
@@ -37,5 +55,6 @@ const PaymentMethod = () => (
     <Sidebar />
   </div>
 );
+
 
 export default PaymentMethod;
