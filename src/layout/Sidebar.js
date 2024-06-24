100119cce5c567a584f96f508ad4841334a28d2a
viilchez <167554276+viilchez@users.noreply.github.com>
2024-06-24 19:05:41 +0000

    commit

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import logoNegro from '../assets/logo-pagalo.svg';
+import logo from '../assets/logo.svg'
 
 const Sidebar = () => {
     return (
       <div className="sidebar">
         <div className="logo-container">
-          <img src={logoNegro} alt="Logo de tu página" height="50" style={{ marginLeft: '0px' }} />
+          <img src={logo} alt="Logo de tu página" height="70" style={{ marginLeft: '30px' }} />
         </div>
-        <div className="postula">
-          <span>SIDEBAR</span>
-        </div>
+        <div className="sidebar-rectangle1">
+          <h1>Preguntas Frecuentes</h1>
+        </div> 
+        <div className="sidebar-rectangle2">
+        <h1>Conoce cómo realizar tus trámites</h1>
+        </div> 
+        <div className="sidebar-rectangle3">
+        <h1>Tasas y servicios más buscados en Págalo.pe</h1>
+        </div> 
+        <div className="sidebar-rectangle4">
+        <h1>Tu opinión es importante</h1>
+        </div> 
+        <div className="sidebar-rectangle5">
+        <h1>Términos y Condiciones</h1>
+        </div> 
       </div>
     );
   };
